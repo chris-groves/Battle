bc773d152d78dd08e8ea29b76948aca4dae01b3d
Chris Groves <chris_groves87@hotmail.co.uk>
2019-06-14 15:18:28 +0100

    add test helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,8 @@
+require_relative 'web_helpers'
+
 feature 'Enter names' do
   scenario 'Players can fill in their names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Leon'
-    fill_in :player_2_name, with: 'Chris'
-    click_button 'Submit'
-    expect(page).to have_content 'Leon vs. Chris'
+    sign_in_and_play
+    expect(page).to have_content 'Chris vs. Leon'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Hit Points' do
   scenario 'Player 1 can see Player 2\'s hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Chris'
-    fill_in :player_2_name, with: 'Leon'
-    click_button 'Submit'
+    sign_in_and_play
   expect(page).to have_content 'Leon - 100HP'
   end
 end
